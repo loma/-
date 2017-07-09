4fb61582fa5eb2b5933e16d5623a392fc6d149c9
loma <57.loma@gmail.com>
2017-07-09 16:25:17 +0700

    fixed adv size

--- android/app/src/main/java/com/borktor_new/MainApplication.java
+++ android/app/src/main/java/com/borktor_new/MainApplication.java
@@ -3,6 +3,7 @@ package com.borktor_new;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.evollu.react.fcm.FIRMessagingPackage;
 import com.sbugert.rnadmob.RNAdMobPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.github.xinthink.rnmk.ReactMaterialKitPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FIRMessagingPackage(),
             new RNAdMobPackage(),
             new RNFetchBlobPackage(),
             new ReactMaterialKitPackage(),
